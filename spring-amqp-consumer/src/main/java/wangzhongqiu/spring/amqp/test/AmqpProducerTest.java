b6e8731d792ea8fde634e913e7547360ea406b4c
wangzhongqiu <zhongqiu.wang@hoomsun.com>
2017-11-02 15:49:57 +0800

    first version

--- spring-amqp-consumer/src/main/java/wangzhongqiu/spring/amqp/test/AmqpProducerTest.java
+++ spring-amqp-consumer/src/main/java/wangzhongqiu/spring/amqp/test/AmqpProducerTest.java
@@ -2,8 +2,6 @@ package wangzhongqiu.spring.amqp.test;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import wangzhongqiu.spring.amqp.CommonProducer;
-import wangzhongqiu.spring.core.amqp.ConsumerMessageDTO;
 
 /**
  * @author wangzhongqiu
@@ -12,8 +10,6 @@ import wangzhongqiu.spring.core.amqp.ConsumerMessageDTO;
 public class AmqpProducerTest {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:spring-amqp.xml");
-        final CommonProducer commonProducer = applicationContext.getBean(CommonProducer.class);
-        ConsumerMessageDTO consumerMessageDTO = new ConsumerMessageDTO();
-        commonProducer.send(consumerMessageDTO);
+        System.out.println("111");
     }
 }
