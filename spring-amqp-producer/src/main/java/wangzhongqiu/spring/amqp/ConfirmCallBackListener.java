d43ef933da080ac607ba1371367e617cc5166be3
leon66666 <1902518544@qq.com>
2017-11-10 11:59:48 +0800

    first version

--- spring-amqp-producer/src/main/java/wangzhongqiu/spring/amqp/ConfirmCallBackListener.java
+++ spring-amqp-producer/src/main/java/wangzhongqiu/spring/amqp/ConfirmCallBackListener.java
@@ -1,5 +1,7 @@
 package wangzhongqiu.spring.amqp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.rabbit.support.CorrelationData;
 import org.springframework.stereotype.Service;
@@ -9,10 +11,12 @@ import org.springframework.stereotype.Service;
  *         Created on 2017/10/31.
  * @description:继承RabbitTemplate.ConfirmCallback,消息确认监听器
  */
+@Service
 public class ConfirmCallBackListener implements RabbitTemplate.ConfirmCallback {
+    private Logger log = LoggerFactory.getLogger(CommonProducer.class);
 
     @Override
     public void confirm(CorrelationData correlationData, boolean ack, String cause) {
-
+        log.info("收到回调");
     }
 }
--- spring-amqp-producer/src/main/java/wangzhongqiu/spring/amqp/ReturnCallBackListener.java
+++ spring-amqp-producer/src/main/java/wangzhongqiu/spring/amqp/ReturnCallBackListener.java
@@ -1,17 +1,22 @@
 package wangzhongqiu.spring.amqp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Service;
 
 /**
  * @author wangzhongqiu
  *         Created on 2017/10/31.
  * @description:继承RabbitTemplate.ReturnCallback,消息发送失败返回监听器
  */
+@Service
 public class ReturnCallBackListener implements RabbitTemplate.ReturnCallback {
+    private Logger log = LoggerFactory.getLogger(CommonProducer.class);
 
     @Override
     public void returnedMessage(Message message, int replyCode, String replyText, String exchange, String routingKey) {
-
+        log.info("收到回调");
     }
 }
