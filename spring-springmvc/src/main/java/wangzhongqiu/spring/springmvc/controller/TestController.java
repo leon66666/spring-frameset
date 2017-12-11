34d21af7d029858f95dfb60dd49a9cda648adc10
leon66666 <1902518544@qq.com>
2017-12-11 21:10:05 +0800

    first version

--- spring-springmvc/src/main/java/wangzhongqiu/spring/springmvc/controller/TestController.java
+++ spring-springmvc/src/main/java/wangzhongqiu/spring/springmvc/controller/TestController.java
@@ -1,8 +1,11 @@
 package wangzhongqiu.spring.springmvc.controller;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import wangzhongqiu.spring.mybatis.annotation.ReadOnlyDataSource;
 import wangzhongqiu.spring.springmvc.annotation.Maintain;
@@ -14,11 +17,16 @@ import wangzhongqiu.spring.springmvc.annotation.Maintain;
 @Controller
 @RequestMapping(value = "/test")
 public class TestController extends BaseController {
+    private static Log logger = LogFactory.getLog(TestController.class);
+
     @RequestMapping(value = "/readOnly")
     @ResponseBody
     @Maintain
     @ReadOnlyDataSource
-    public ModelMap readOnly(String str) {
+    public ModelMap readOnly(@RequestParam(value = "pageNumber", required = false) String pageNumber,
+                             @RequestParam(value = "pageSize", required = false) String pageSize,
+                             @RequestParam(value = "version", required = false, defaultValue = "1.0") String version
+    ) {
         ModelMap mm = new ModelMap();
         try {
             mm.addAttribute("message", "success");
